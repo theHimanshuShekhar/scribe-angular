960e3f609e22e6b10d8c0ca15b90c53f4ed3299a
Himanshu Shekhar <hemanshoe1234@gmail.com>
2017-11-16 10:15:05 +0530

    time conversion for posts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -21,7 +21,42 @@ export class PostsComponent implements OnInit {
     const prevDate = date;
     const newDate = new Date();
     const milliseconds: number = newDate.getTime() - prevDate.getTime();
-    return Math.trunc(milliseconds / 60000);
+    const minutes = Math.trunc(milliseconds / 60000);
+    let hours;
+    let days;
+    let months;
+    let years;
+    if (minutes < 59) {
+      if (minutes < 1) {
+        return 'just now';
+      }
+      return minutes + ' minutes ago';
+    } else {
+      hours = Math.trunc(minutes / 60);
+      if (hours < 2) {
+        return  hours + ' hour ago';
+      }
+      if (hours > 24 ) {
+        days = Math.trunc(hours / 24);
+        return days + ' days ago';
+      }
+      if(hours > 730) {
+        months = Math.trunc(hours / 730);
+        if (months > 12) {
+          years = Math.trunc(months / 12);
+          if (years < 2) {
+            return years + ' year ago';
+          } else {
+            return years + ' years ago';
+          }
+        }
+        if (months < 2) {
+          return months + ' month ago';
+        }
+        return months + ' months ago';
+      }
+      return  hours + ' hours ago';
+    }
   }
 
   ngOnInit() {
