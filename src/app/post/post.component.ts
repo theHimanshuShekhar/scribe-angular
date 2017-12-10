0f4a596dd7bd905956bf9e99352c60ef6a582fcb
Himanshu Shekhar <hemanshoe1234@gmail.com>
2017-12-11 05:12:54 +0530

    added styling and formatting to Post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,3 +1,4 @@
+import { DateFormatPipe } from './../services/date.pipe';
 import { Router } from '@angular/router';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Input } from '@angular/core/src/metadata/directives';
@@ -10,8 +11,15 @@ import { Input } from '@angular/core/src/metadata/directives';
 })
 export class PostComponent implements OnInit {
 
+  author: string = 'author';
+  authorPhotoURL = '../../assets/images/default-profile.jpg';
+  username: string = 'username';
+  body: string = 'Body';
+  date = new Date();
+
   constructor(
-    private router: Router
+    private router: Router,
+    private dateFormatPipe: DateFormatPipe,
   ) { }
 
   ngOnInit() {
@@ -19,6 +27,30 @@ export class PostComponent implements OnInit {
 
   public sendToProfile(username) {
     this.router.navigateByUrl('user/' + username);
+  }
+
+  public getDate(date) {
+    setTimeout(500);
+    if (date) {
+      const prevDate = date;
+      const newDate = new Date();
+      const milliseconds: number = newDate.getTime() - prevDate.getTime();
+      const minutes = Math.trunc(milliseconds / 60000);
+      let hours;
+      if (minutes < 59) {
+        if (minutes < 1) {
+          return 'just now';
+        }
+        return minutes + 'm';
+      } else {
+        hours = Math.trunc(minutes / 60);
+        if (hours >= 1 && hours < 24) {
+          return hours + 'h';
+        } else {
+          return this.dateFormatPipe.transform(prevDate);
+        }
+      }
+    }
   }
 
 }
