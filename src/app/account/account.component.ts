896a3dfb51b15e22e256d049c1381ecd58330405
Himanshu Shekhar <hemanshoe1234@gmail.com>
2017-12-27 06:04:36 +0530

    updated to account page to allow changing user photoURL

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,8 +1,11 @@
+import { UploadService } from './../services/upload.service';
 import { Router } from '@angular/router';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { Upload } from '../classes/upload';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-account',
@@ -19,17 +22,21 @@ export class AccountComponent implements OnInit {
   userCollection: AngularFirestoreCollection<any>;
   userObs: Observable<any>;
 
-  constructor(private auth: AuthService, private router: Router, private afs: AngularFirestore) { }
+  constructor(private auth: AuthService, private router: Router, private afs: AngularFirestore, private uploadService: UploadService) { }
+
+  selectedFiles: FileList;
+  currentUpload: Upload;
+  progressname;
 
   ngOnInit() {
     this.auth.getAuthState().subscribe(user => {
       if (user) {
         this.uid = user.uid;
         this.userCollection = this.afs.collection('users', ref => ref.where('uid', '==', this.uid));
-        this.userCollection.valueChanges().subscribe(user => {
-          this.displayname = user[0].displayName;
-          this.username = user[0].userName;
-          this.status = user[0].status;
+        this.userCollection.valueChanges().subscribe(userlist => {
+          this.displayname = userlist[0].displayName;
+          this.username = userlist[0].userName;
+          this.status = userlist[0].status;
         });
       } else {
         this.router.navigateByUrl('/home');
@@ -39,6 +46,31 @@ export class AccountComponent implements OnInit {
 
   public update() {
     this.auth.updateUser(this.displayname, this.username, this.status);
-    this.router.navigateByUrl('/home');
+    if (this.selectedFiles) {
+      if (this.selectedFiles[0].size <= 300000) {
+        this.uploadSingle();
+      } else {
+        console.log('invalid file size');
+      }
+    }
+  }
+
+  detectFiles(event) {
+    if (event.target.files[0]) {
+      this.selectedFiles = event.target.files;
+      this.progressname = this.selectedFiles[0].name;
+    }
+  }
+
+  uploadSingle() {
+    if (this.selectedFiles[0].type === 'image/png' || this.selectedFiles[0].type === 'image/jpeg') {
+      const file = this.selectedFiles.item(0);
+      this.currentUpload = new Upload(file);
+      const useruid = this.auth.getUid();
+      this.currentUpload.name = 'dp';
+      this.uploadService.pushUpload(this.currentUpload, 'user', useruid);
+    } else {
+      console.log('Invalid format');
+    }
   }
 }
