0b77d425b26274d9035682dc6e386bba2f5b90ea
theHimanshuShekhar <theHimanshuShekhar@github.com>
2018-01-08 10:44:49 +0530

    added user info on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-
+import { UserService } from '../services/user.service';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,10 +10,26 @@ import { AuthService } from '../services/auth.service';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private userService: UserService
   ) { }
 
+  displayName = 'Display Name';
+  userName = 'username'
+  photoURL = '../../assets/images/default-profile.jpg';
+
   ngOnInit() {
+    this.auth.getAuthState().subscribe(
+      user => {
+        if (user) {
+          this.userService.retrieveUserDocument(user.uid).subscribe(
+            userDoc => {
+              this.displayName = userDoc.displayName;
+              this.userName = userDoc.userName;
+              this.photoURL = userDoc.photoURL;
+            });
+        }
+      });
   }
 
   logout() {
