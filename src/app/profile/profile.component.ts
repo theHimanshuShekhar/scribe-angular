cd1ebfa4f1f504605463808cc0f1a042f44ea660
Himanshu Shekhar <hemanshoe1234@gmail.com>
2018-01-10 00:27:09 +0530

    added profile component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,42 @@
+import { UserService } from './../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+  ) { }
+
+  displayName;
+  userName;
+  photoURL = '../../assets/images/default-profile.jpg';
+  status;
+  joinDate = 'May 2009';
+  userid = null;
+  showInvalid: boolean;
+
+  ngOnInit() {
+    this.userService.retrieveUserDocumentFromUsername(this.router.url.slice(6)).subscribe(
+      user => {
+        if (user[0]) {
+          console.log(user[0]);
+          user = user[0]
+          this.displayName = user.displayName;
+          this.userName = user.userName;
+          this.status = user.status;
+          this.photoURL = user.photoURL;
+          this.userid = user.uid;
+        } else {
+          this.showInvalid = true;
+        }
+    });
+  }
+
+}
