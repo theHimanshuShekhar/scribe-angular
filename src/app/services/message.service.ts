2516b2c63a205c547486dc90f1346e3ed21d75ce
theHimanshuShekhar <hemanshoe1234@gmail.com>
2018-04-12 23:28:11 +0530

    message service misc

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class MessageService {
 
-  constructor() { }
+  constructor(
+    private afs: AngularFirestore,
+    private auth: AuthService
+  ) { }
+
+  getProfileUserRooms(uid) {
+    return this.afs.collection('/users/' + uid + '/messaging').valueChanges();
+  }
+
+  getCommonRoom(uid) {
+    this.getProfileUserRooms(uid).subscribe(ProfileUsersChatrooms => {
+      if (ProfileUsersChatrooms) {
+        ProfileUsersChatrooms.forEach(room:any => {
+          this.getChatroom(room.rid).subscribe(
+            chatroom => {
+              if (chatroom) {
+                
+              }
+            });
+        });
+      }
+    });
+  }
+
+  getChatroom(rid) {
+    return this.afs.doc('messaging/' + rid).valueChanges();
+  }
+
+  createChatroom(profileuid) {
+    this.auth.getAuthState().subscribe(
+      curruser => {
+        const rid = this.afs.createId();
+        const roomData = {
+          rid: rid,
+          user1: curruser.uid,
+          user2: profileuid
+        };
+        this.afs.doc('messaging/' + rid).set(roomData);
+      });
+  }
+  
 
 }
