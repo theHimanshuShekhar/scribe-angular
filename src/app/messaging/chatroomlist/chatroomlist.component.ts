45fbc72003f5d441e2ae86a168bb2e7834ea2e37
Himanshu Shekhar <hemanshoe1234@gmail.com>
2018-04-17 02:32:51 +0530

    added history state to chatroom

--- src/app/messaging/chatroomlist/chatroomlist.component.ts
+++ src/app/messaging/chatroomlist/chatroomlist.component.ts
@@ -1,8 +1,9 @@
 import { UserService } from './../../services/user.service';
 import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { MessagingComponent } from '../messaging.component';
 import { ChatroomComponent } from '../chatroom/chatroom.component';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-chatroomlist',
@@ -18,10 +19,21 @@ export class ChatroomlistComponent implements OnInit {
   photoURL;
   roomData;
 
+  modalRef;
+  closeResult;
+
   constructor(
     private userService: UserService,
-    private modalService: NgbModal
-  ) { }
+    private modalService: NgbModal,
+    private location: PlatformLocation
+  ) {
+    location.onPopState((event) => {
+      // ensure that modal is opened
+      if (this.modalRef !== undefined) {
+          this.modalRef.close();
+      }
+    });
+   }
 
   ngOnInit() {
     this.userService.retrieveUserDocumentFromID(this.room.uid).subscribe(user => {
@@ -37,10 +49,27 @@ export class ChatroomlistComponent implements OnInit {
   }
 
   openChatroom() {
-    this.modalService.open(this.modalContent, {
+    this.modalRef = this.modalService.open(this.modalContent, {
       size: 'lg',
       windowClass: 'modal-style'
     });
+    history.pushState(null, null, 'chatroom/' + this.room.rid);
+    this.modalRef.result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    history.back();
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
