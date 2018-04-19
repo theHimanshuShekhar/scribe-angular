3088006adc8e414f6c7a598cc7d832f7e29a1368
Himanshu Shekhar <hemanshoe1234@gmail.com>
2018-04-20 01:31:31 +0530

    completed about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,8 @@
+import { GroupService } from './../services/group.service';
+import { PostsService } from './../services/posts.service';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-about',
@@ -8,61 +11,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class AboutComponent implements OnInit {
 
+  mostCommented;
+  mostLiked;
+  mostFollowed;
+  mostSubbed;
+
   constructor(
-    private title: Title
+    private title: Title,
+    private postService: PostsService,
+    private userService: UserService,
+    private groupService: GroupService
   ) { }
-
-  // Line Chart Data
-  public lineChartData:Array<any> = [
-    {data: [0, 3, 5, 15, 20, 22, 34], label: 'Posts'},
-    {data: [0, 1, 2, 3, 4, 5, 8], label: 'Groups'},
-    {data: [2, 7, 8, 10, 12, 14, 15], label: 'Users'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-  public lineChartColors:Array<any> = [
-    { 
-      backgroundColor: 'rgba(0,0,255,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { 
-      backgroundColor: 'rgb(0,0,0,0.4)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { 
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
-
-  public chartClicked(e:any):void {
-    
-  }
- 
-  public chartHovered(e:any):void {
-    
-  }
- 
-
 
   ngOnInit() {
     this.title.setTitle('Scribe | About');
+    this.postService.getMostLikedPosts().subscribe(posts => this.mostLiked = posts);
+    this.postService.getMostCommentedPosts().subscribe(posts => this.mostCommented = posts);
+    this.userService.getSuggestedUsers().subscribe(users => this.mostFollowed = users);
+    this.groupService.getMostSubbed().subscribe(groups => this.mostSubbed = groups);
   }
 }
